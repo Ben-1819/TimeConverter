8404761ff2e75791a65ec6f28ebc64be1aabdb2f
Ben-1819 <ben.brown20332@gmail.com>
2026-06-01 14:28:23 +0100

    feat: Created model and migration for batches table

---
+++ TimeConverterBackend/database/migrations/2026_06_01_132426_create_batches_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('batches', function (Blueprint $table) {
+            $table->id();
+            $table->integer('batch_number')->unique();
+            $table->date('date_started');
+            $table->date('date_completed')->nullable();
+            $table->boolean('complete')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('batches');
+    }
+};
