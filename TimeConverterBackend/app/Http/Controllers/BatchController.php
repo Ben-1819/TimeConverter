6f8067b0ca8decedcfd21c78491cc862d1004c79
Ben-1819 <ben.brown20332@gmail.com>
2026-06-07 17:44:53 +0100

    feat: Set up controller for batch model

---
+++ TimeConverterBackend/app/Http/Controllers/BatchController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Batch;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Carbon\Carbon;
+
+class BatchController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        Log::info("BatchController index method running");
+        $batches = Batch::all();
+        return response()->json([
+            "batches" => $batches,
+            "successMessage" => "All batches retrieved successfully"
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Log::info("BatchController store method running");
+
+        $request->validated();
+
+        $batch = Batch::create([
+            'batch_number' => $request['batch_number'],
+            'date_started' => Carbon::now(),
+            'date_completed' => null,
+            'complete' => false,
+        ]);
+
+        return response()->json([
+            "batch" => $batch,
+            "successMessage" => "Batch created successfully"
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        Log::info("BatchController show method running");
+
+        $batch = Batch::find($id);
+
+        return response()->json([
+            "batch" => $batch,
+            "successMessage" => "Batch retrieved successfully"
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        Log::info("BatchController update method running");
+
+        $request->validated();
+
+        $oldBatch = Batch::find($id);
+
+        $updatedBatch = Batch::where('id', $id)->update([
+            'batch_number' => $request['batch_number'] ?? $oldBatch->batch_number,
+            'date_started' => $request['date_started'] ?? $oldBatch->date_started,
+            'date_completed' => $request['date_completed'] ?? $oldBatch->date_completed,
+            'complete' => $request['complete'] ?? $oldBatch->complete,
+        ]);
+
+        return response()->json([
+            'updatedBatch' => $updatedBatch,
+            'successMessage' => 'Batch updated successfully'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        Log::info("BatchController destroy method running");
+
+        Batch::where('id', $id)->delete();
+
+        return response()->json([
+            'successMessage' => 'Batch deleted successfully',
+        ]);
+    }
+}
