fc543508098ebe2ddbb55821da0508e70fd333d9
Ben-1819 <ben.brown20332@gmail.com>
2026-06-17 09:29:26 +0100

    feat: Created the routes

--- TimeConverterBackend/routes/api.php
+++ TimeConverterBackend/routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BatchController;
+use App\Http\Controllers\TimeController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::controller(BatchController::class)->prefix('batches')->group(function () {
+    Route::get('/', 'index');
+    Route::post('/', 'store');
+    Route::get("/{id}", 'show');
+    Route::put("/{id}", 'update');
+    Route::delete("/{id}", 'destroy');
+});
+
+Route::controller(TimeController::class)->prefix('times')->group(function () {
+    Route::get('/', 'index');
+    Route::post('/', 'store');
+    Route::get("/batch/{id}", 'timesByBatch');
+    Route::get("/{id}", 'show');
+    Route::put("/{id}", 'update');
+    Route::delete("/{id}", 'destroy');
+});
