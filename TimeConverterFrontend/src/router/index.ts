18a520c3c2635ae779fc772b0841f82819b6925c
Ben-1819 <ben.brown20332@gmail.com>
2026-06-22 08:54:27 +0100

    feat: Registered batch menu to the router

--- TimeConverterFrontend/src/router/index.ts
+++ TimeConverterFrontend/src/router/index.ts
@@ -1,9 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
+import BatchMenu from '@/views/BatchMenu.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{ path: '/', name: 'Home', component: Home }],
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
+    },
+    {
+      path: '/BatchMenu',
+      name: 'Batch Menu',
+      component: BatchMenu,
+    },
+  ],
 });
 
 export default router;
