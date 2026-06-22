affa248cbbd80f6275caa49a15dcfad6ef0ab94d
Ben-1819 <ben.brown20332@gmail.com>
2026-06-22 08:54:10 +0100

    feat: Created batch menu view

---
+++ TimeConverterFrontend/src/views/BatchMenu.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import Sidebar from '@/components/Sidebar.vue';
+import NavigationButton from '@/components/NavigationButton.vue';
+
+const router = useRouter();
+</script>
+<template>
+  <SidebarProvider>
+    <Sidebar />
+    <main class="w-full">
+      <div class="flex justify-start">
+        <SidebarTrigger />
+      </div>
+      <div class="flex justify-center">
+        <h1 class="text-xl pb-10 text-green-600">Batch Menu</h1>
+      </div>
+      <div class="flex justify-center">
+        <div class="grid grid-cols-2 gap-8">
+          <NavigationButton>All Batches</NavigationButton>
+          <NavigationButton>Create Batch</NavigationButton>
+        </div>
+      </div>
+    </main>
+  </SidebarProvider>
+</template>
